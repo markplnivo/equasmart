662fe5993bd468be1d7db98e79b86b69ab4c9b1b
Mark13Codes <markfrederickr@gmail.com>
2024-11-02 15:11:09 +0800

    Update admin_menu.php
    updated admin menu fixed the sidebar menu

--- html/admin/admin_menu.php
+++ html/admin/admin_menu.php
@@ -1,3 +1,4 @@
+<!--github by jayson -->
 <?php ob_start(); ?>
 <?php
 include "../session_handler.php";
@@ -23,7 +24,12 @@ if ($_SESSION['position'] != 'admin') {
 </head>
 
 <style>
-  
+ *, body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', 'Arial', sans-serif;
+} 
 .custom-menu {
     width: 200px;
     height: auto;
@@ -34,22 +40,18 @@ if ($_SESSION['position'] != 'admin') {
     display: flex;
     flex-direction: column;
 }
-
 .custom-menu ul {
     list-style-type: none;
     padding: 0;
     margin: 7px 0;
   }
-
 .custom-menu ul.logout {
     margin-bottom: 30%;
   }
-
 .custom-menu li {
     width: 100%;
     color: white;
 }
-
 .custom-menu li a {
     display: flex;
     align-items: center;
@@ -58,7 +60,6 @@ if ($_SESSION['position'] != 'admin') {
     padding: 15px;
     background-color: mediumaquamarine;
   }
-
 .custom-menu li:hover a {
     background-color: mediumseagreen;
     color: white;
@@ -77,17 +78,79 @@ if ($_SESSION['position'] != 'admin') {
   }
 
 #menuLogo {
-    margin-top: 15px;
+    margin-top: 5%;
     align-self: center;
     width: 50%;
 }
 
 .logout button {
+    display: flex;
     background: none;
     border: none;
     color: white;
     font-size: 1.2em;
     cursor: pointer;
+    height: 100%;
+    width: 100%;
+    padding: 15px;
+}
+.logout li a{
+    padding: 0;
+}
+.logout span{
+    align-self: left;
+    font-size: 1.2em;
+    margin-right: 10%;
+}
+.custom-menu div{
+  margin: 3% auto;
+  padding: 15px;
+}
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
+
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    transition: .4s;
+    border-radius: 34px;
+    margin: 3% auto;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: .4s;
+    border-radius: 50%;
+  }
+
+  input:checked+.slider {
+    background-color: #4CAF50;
+  }
+
+  input:checked+.slider:before {
+    transform: translateX(26px);
+
 }
 
 /* Mobile responsiveness */
@@ -121,74 +184,69 @@ if ($_SESSION['position'] != 'admin') {
         font-size: 1em;
   }
 
-  /* Toggle switch style */
+  /* Updated Toggle switch style for smaller screens */
+  .custom-menu div{
+        margin: 2% auto;
+        padding: 8px;
+    }
   .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
+        width: 50px;
+        height: 28px;
   }
 
   .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: .4s;
-    border-radius: 34px;
+        background-color: #ddd;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+        transition: background-color 0.4s, box-shadow 0.4s;
   }
 
   .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
+        height: 20px;
+        width: 20px;
+        left: 3px;
     bottom: 4px;
-    background-color: white;
-    transition: .4s;
-    border-radius: 50%;
   }
 
-  input:checked+.slider {
+    input:checked + .slider {
     background-color: #4CAF50;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   }
 
-  input:checked+.slider:before {
-    transform: translateX(26px);
-
+    input:checked + .slider:before {
+        transform: translateX(22px);
   }
 }
 </style>
 
 <body>
 <div class="custom-menu">
+<img src="../images/equasmartlogo_croppedlogo.png" alt="Logo" id="menuLogo">
     <!-- <input id="menu_toggle" type="checkbox" />
     <label class="menu_btn" for="menu_toggle">
       <span></span>
     </label> -->
 
-    <ul class="menu_box">
-      <li><img src="../images/equasmartlogo_croppedlogo.png" alt="Logo" width="100" height="100"></li>
+    <ul>
       <li>
-        <div class="menu_spacer"></div>
+      <a href="admin_home.php">
+        <span class="icon fa fa-home"></span>
+        <span class="nav-text">Home</span>
+      </a>
       </li>
-      <li class="icon fa fa-home"><a class="menu_item" href="admin_home.php"><span class="nav-text">Home</span></a></li>
-      <li class="icon fa fa-recycle"><a class="menu_item" href="admin_recyclebin.php"><span class="nav-text">Recycle Bin</span></a></li>
-      <li class="icon fa fa-users"><a class="menu_item" href="admin_userlist.php"><span class="nav-text">User List</span></a></li>
+      <li>
+      <a href="admin_recyclebin.php">
+        <span class="icon fa fa-recycle"></span>
+        <span class="nav-text">Recycle Bin</span>
+      </a>
+      </li>
+      <li>
+      <a href="admin_userlist.php">
+        <span class="icon fa fa-users"></span>
+        <span class="nav-text">User List</span>
+      </a>
+      </li>
       <!-- <li><a class="menu_item" href="admin_invite.php">Invite User</a></li> -->
-      <li>
-        <div class="menu_spacer"></div>
-      </li>
+      
       <!-- Toggle slider for ngrok control -->
       <div>
         <label class="switch">
@@ -197,25 +255,28 @@ if ($_SESSION['position'] != 'admin') {
         </label>
       <p id="statusMessage"></p>
       </div>
-
-      <li>
-        <form action="admin_menu.php" method="post">
-          <div class="logButton">
-          <span class="icon fa fa-power-off"></span>
-            <button id="buttonlogout" type="submit" name="logoutButton" class="menu_item">Logout</button>
-          </div>
-        </form>
-      </li>
     </ul>
-  </div>
 
+    <ul class="logout">
+      <li>
+        <a href="#">
+        <form action="admin_menu.php" method="post">
+          <button name="logout">
+          <span class="icon fa fa-power-off"></span>
+            <span class="nav-text">Logout</span>
+          </button>
   <?php
-  if (isset($_POST['logoutButton'])) {
+            if (isset($_POST['logout'])) {
     logoutUser();
     header("Location: ../login_page.php");
     exit();
   }
   ?>
+            </a>
+        </li>
+      </ul>
+    </form>
+  </div>
 
   <!-- <script>
     // Existing code
