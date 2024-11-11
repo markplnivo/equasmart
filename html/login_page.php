b5675c297b575fdf20bbcbade20217b91a37a1c4
Mark13Codes <markfrederickr@gmail.com>
2024-11-11 13:04:45 +0800

    Update login_page
    added links css style link, fontawesome link and equasmart icon logo

--- html/login_page.php
+++ html/login_page.php
@@ -9,9 +9,12 @@ include "session_handler.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="manifest" href="manifest.json">
+    <link rel="shortcut icon" href="images/equasmartlogo_croppedlogo.png" type="image/svg+xml">
+    <link rel="stylesheet" href="css/css/all.min.css">
+    <link rel="stylesheet" href="css/css/fontawesome.min.css">
+    <title>Login</title>
 
     <style>
         :root {
